61acfc8ee7b49ade8ec132fd13f569c3b75e49e8
Mojtaba Eshghie <eshghie@kth.se>
2025-04-16 10:18:56 +0200

    Enhancement: Removing accidentally added sindi folder #50

--- sindi/src/sindi/__init__.py
+++
@@ -1,2 +0,0 @@
-# filepath: /Users/mojtabaeshghie/projects/SINA/src/sindi/__init__.py
-# This file is intentionally left blank.
--- sindi/src/sindi/main.py
+++
@@ -1,5 +0,0 @@
-def main():
-    print("Welcome to SInDi: Semantic Invariant Differencing for Solidity Smart Contracts")
-
-if __name__ == "__main__":
-    main()
